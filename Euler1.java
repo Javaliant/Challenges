e6f06d8658a7d546f6ab0b5d554ed43cc091e9f3
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-07 13:18:16 -0500

    Create Euler1.java

---
+++ Euler1.java
@@ -0,0 +1,23 @@
+/* Author: Luigi Vincent
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+public class Euler1 {
+	public static void main(String[] args) {
+		int result = 0;
+		long time = System.nanoTime();
+
+ 		for (int i = 3; i < 1000; i++) {
+ 			if (i % 3 == 0 || i % 5 == 0) {
+ 				result += i;
+ 			}
+ 		}
+
+		System.out.print("Result: " + result +
+			".\nTime used for calculation in nanoseconds: " +
+			(System.nanoTime() - time) + ".");
+    }
+}
