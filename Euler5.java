6c961c9595b86fc95f6d4c29eabfe2a0a10890a4
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-08 08:05:50 -0500

    Update Euler5.java

--- Euler5.java
+++ Euler5.java
@@ -7,13 +7,12 @@ What is the smallest positive number that is evenly divisible by 1-20?
 public class Euler5 {
 	public static void main(String[] args) {
 		long startTime = System.nanoTime();
-		int result = 0;
+		// Multiplying by primes for a starting point * 2 for evenness
+		int givenTimesPrimes = 2520 * 2 * 11 * 13 * 17 * 19,
+			result = givenTimesPrimes;
 
-		for (int i = 2520; i > 0; i += 2520) {
-			if (isSmallestMultiple(i)) {
-				result = i;
-				break;
-			}
+		while (!isSmallestMultiple(result)) {
+			result += givenTimesPrimes;
 		}
 
 		System.out.print("Result: " + result +
@@ -21,25 +20,12 @@ public class Euler5 {
 			(System.nanoTime() - startTime) + ".");
 	}
 
-	public static boolean isSmallestMultiple(int i) {
-		return i % 2 == 0 &&
-			i % 3 == 0 &&
-			i % 4 == 0 &&
-			i % 5 == 0 &&
-			i % 6 == 0 &&
-			i % 7 == 0 &&
-			i % 8 == 0 &&
-			i % 9 == 0 &&
-			i % 10 == 0 &&
-			i % 11 == 0 &&
-			i % 12 == 0 &&
-			i % 13 == 0 &&
-			i % 14 == 0 &&
-			i % 15 == 0 &&
-			i % 16 == 0 &&
-			i % 17 == 0 &&
-			i % 18 == 0 &&
-			i % 19 == 0 &&
-			i % 20 == 0;
+	public static boolean isSmallestMultiple(int n) {
+		for (int i = 11; i <= 20; i++) {
+			if (n % i != 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
