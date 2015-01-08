ca3b0877caf83099c272edf799ad15c336c6f565
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-08 01:14:46 -0500

    Create Euler3.java

---
+++ Euler3.java
@@ -0,0 +1,25 @@
+/* Author: Luigi Vincent
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143?
+*/
+
+public class Euler3 {
+	public static void main(String[] args) {
+		long startTime = System.nanoTime(),
+			result = 600851475143L,
+			divisor = 2L;
+
+		while (divisor * divisor <= result) {
+			if (result % divisor == 0) {
+				result /= divisor;
+			} else {
+				divisor++;
+			}
+		}
+
+		System.out.print("Result: " + result +
+			".\nTime used for calculation in nanoseconds: " +
+			(System.nanoTime() - startTime) + ".");
+	}
+}
