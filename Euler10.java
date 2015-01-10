420366fb299bde5e242b6bc1ee11ce5050cf6550
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-09 21:43:57 -0500

    Update Euler10.java

--- Euler10.java
+++ Euler10.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 
 public class Euler10 {
 	public static void main(String[] args) {
+		final long START = System.nanoTime();
 		long result = 0;
-		long time = System.nanoTime();
 
 		ArrayList<Integer> nums = new ArrayList<>();
 
@@ -18,27 +18,31 @@ public class Euler10 {
 				nums.add(i);
 			}
 		}
+		
 		for (int n : nums) {
 			result += n;
 		}
+		
+		final long END = System.nanoTime();
 
 		System.out.print("Result: " + result +
 			".\nTime used for calculation in nanoseconds: " +
-			(System.nanoTime() - time) + ".");
-    }
-
-    public static boolean isPrime(long n) {
-    	if (n == 1) { return false; }
-    	else if (n < 4) { return true; }
-   		else if (n % 2 == 0 || n % 3 == 0) {
-        return false;
-    	} else {
-        	for (int i = 5; i * i <= n; i += 6) {
-            	if (n % i == 0 || n % (i + 2) == 0) {
-                	return false;
-            	}
-        	}
-        return true;
+			(END - START) + "."
+		);
     	}
+
+	public static boolean isPrime(long n) {
+    		if (n == 1) { return false; }
+    		else if (n < 4) { return true; }
+   		else if (n % 2 == 0 || n % 3 == 0) {
+        		return false;
+    		} else {
+        		for (int i = 5; i * i <= n; i += 6) {
+            			if (n % i == 0 || n % (i + 2) == 0) {
+                			return false;
+            			}
+        		}
+        		return true;
+    		}
 	}
 }
