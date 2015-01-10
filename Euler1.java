6a0c388f01a7b715f44a9f47d6846d4e990b7e3d
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-09 21:25:44 -0500

    Update Euler1.java

--- Euler1.java
+++ Euler1.java
@@ -8,17 +8,20 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 public class Euler1 {
 	public static void main(String[] args) {
+		final long START = System.nanoTime();
 		int result = 0;
-		long time = System.nanoTime();
-
+		
  		for (int i = 3; i < 1000; i++) {
  			if (i % 3 == 0 || i % 5 == 0) {
  				result += i;
  			}
  		}
+ 		
+ 		final  long END = System.nanoTime();
 
 		System.out.print("Result: " + result +
 			".\nTime used for calculation in nanoseconds: " +
-			(System.nanoTime() - time) + ".");
+			(END - START) + "."
+		);
     }
 }
