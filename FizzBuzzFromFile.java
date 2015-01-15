2892bfad25505cc6163bf3fbcba69b50ff810629
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-15 02:15:59 -0500

    Update FizzBuzzFromFile.java

--- FizzBuzzFromFile.java
+++ FizzBuzzFromFile.java
@@ -14,6 +14,16 @@ You may assume that the input file is formatted correctly and
 the numbers are valid positive integers. 
 Your output should print out one line per set. 
 Ensure that there are no trailing empty spaces in each line you print.
+
+Found on CodeEval.com
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/* Author: Luigi Vincent
+Printing Fizzbuzz via I/O!
 */
 
 import java.io.File;
@@ -22,26 +32,28 @@ import java.util.Scanner;
 
 public class FizzBuzzFromFile {
     public static void main(String[] args) throws FileNotFoundException {
-    	File file = new File(args[0]);
-    	Scanner fileScanner = new Scanner(file);
-    	while (fileScanner.hasNextLine()) {
-    		printFizzBuzz(fileScanner.nextLine());
-    	}
+        File file = new File(args[0]);
+        Scanner fileScanner = new Scanner(file);
+        while (fileScanner.hasNextLine()) {
+            printFizzBuzz(fileScanner.nextLine());
+        }
     }
 
     public static void printFizzBuzz(String line) {
-    	int fizz = Integer.parseInt(line.split(" ")[0]);
-    	int buzz = Integer.parseInt(line.split(" ")[1]);
-    	int limit = Integer.parseInt(line.split(" ")[2]);
-    	
-    	StringBuilder sb = new StringBuilder();
+        int fizz = Integer.parseInt(line.split(" ")[0]);
+        int buzz = Integer.parseInt(line.split(" ")[1]);
+        int limit = Integer.parseInt(line.split(" ")[2]);
+        
+        StringBuilder sb = new StringBuilder();
 
-    	for (int i = 1; i <= limit; i++) {
-    		if (i % fizz == 0) { sb.append("F"); }
-    		if (i % buzz == 0) { sb.append("B"); }
-    		else { if (i % fizz != 0) { sb.append(i); } }
-    		if (i < limit) { sb.append(" "); }
-    	}
-    	System.out.println(sb.toString());
+        for (int i = 1; i <= limit; i++) {
+            boolean appendNum = true;
+            if (i % fizz == 0) { sb.append("F"); appendNum = false; }
+            if (i % buzz == 0) { sb.append("B"); appendNum = false; }
+            if (appendNum) { sb.append(i); } 
+            sb.append(" ");
+        }
+        sb.setLength(sb.length() - 1);
+        System.out.println(sb.toString());
     }
 }
