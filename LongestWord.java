c1cfa4be1555252f0749be6359cf2c2cc259a74d
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-16 10:13:58 -0500

    Create LongestWord.java

---
+++ LongestWord.java
@@ -0,0 +1,40 @@
+/* Author: Luigi Vincent
+Challenge: Print the longest word in a sentence.
+Specifications: Program should accept a file as its input.
+Each line has one or more words, each word separated by a space.
+If the sentence has more than one word of the same length,
+print the first one.
+
+Found on CodeEval
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LongestWord {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File(args[0]);
+		Scanner fileInput = new Scanner(file);
+		while (fileInput.hasNextLine()) {
+			printLongest(fileInput.nextLine());
+		}	
+	}
+
+	private static void printLongest(String file) {
+		System.out.println(getLongest(file));
+	}
+	// Finds and returns the longest word in a string
+	private static String getLongest(String str) {
+		String[] words = str.split(" ");
+		int longestPosition = 0, longestLength = words[0].length();
+
+		for (int i = 1; i < words.length; i++) {
+			if (words[i].length() > longestLength) {
+				longestLength = words[i].length();
+				longestPosition = i;
+			}
+		}
+		return words[longestPosition];
+	}
+}
