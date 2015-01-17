c1ea727b3fa71674e14e85a5e4aa3ef1797b3027
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-17 15:30:51 -0500

    Create DecimalToBinary.java

---
+++ DecimalToBinary.java
@@ -0,0 +1,31 @@
+/* Author: Luigi Vincent
+Challenge: Convert Decimal number to its binary representation.
+
+Specifications: Your program should accept as its first argument a path to a filename.
+Ignore all Empty Lines.
+
+Found on CodeEval
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DecimalToBinary {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File(args[0]);
+		Scanner fileInput = new Scanner(file);
+
+		while(fileInput.hasNextLine()) {
+			printBinaryForm(fileInput.nextLine());
+		}
+	}
+
+	private static void printBinaryForm(String line) {
+		if (!line.isEmpty()) {
+			System.out.println(
+				Integer.toBinaryString(Integer.parseInt(line))
+			);
+		}
+	}
+}
