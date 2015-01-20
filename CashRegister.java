4aa365e847359ce44677c1d3f7c64754e79c04b1
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-20 01:20:12 -0500

    Update CashRegister.java

--- CashRegister.java
+++ CashRegister.java
@@ -38,71 +38,69 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class CashRegister {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File(args[0]);
-        Scanner fileInput = new Scanner(file);
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File(args[0]);
+		Scanner fileInput = new Scanner(file);
 
-        while (fileInput.hasNextLine()) {
-            printCashChange(fileInput.nextLine());
-        }
-    }
+		while (fileInput.hasNextLine()) {
+			printCashChange(fileInput.nextLine());
+		}
+	}
 
-    private static void printCashChange(String line) {
-        float pp = Float.parseFloat(line.split(";")[0]);
-        float ch = Float.parseFloat(line.split(";")[1]);
+	private static void printCashChange(String line) {
+		String[] fields = line.split(";");
 
-        System.out.println(getCashChange(pp, ch));
-    }
+		float pp = Float.parseFloat(fields[0]);
+		float ch = Float.parseFloat(fields[1]);
 
-    private static String getCashChange(float price, float cash) {
-        if (cash < price) { return "ERROR"; }
-        if (cash == price) { return "ZERO"; }
-        float cashBack = cash - price;
+		System.out.println(getCashChange(pp, ch));
+	}
 
+	private static String getCashChange(float price, float cash) {
+		if (cash < price) { return "ERROR"; }
+		if (cash == price) { return "ZERO"; }
+		float cashBack = cash - price;
 
-        StringBuilder change = new StringBuilder();
+		StringBuilder change = new StringBuilder();
 
-        while (cashBack > 0.01f) {
-            if (cashBack >= 100.0f) {
-                change.append("ONE HUNDRED");
-                cashBack -= 100.0f;
-            } else if (cashBack >= 50.0f) {
-                change.append("FIFTY");
-                cashBack -= 50.0f;
-            } else if (cashBack >= 20.0f) {
-                change.append("TWENTY");
-                cashBack -= 20.0f;
-            } else if (cashBack >= 10.0f) {
-                change.append("TEN");
-                cashBack -= 10.0f;
-            } else if (cashBack >= 5.0f) {
-                change.append("FIVE");
-                cashBack -= 5.0f;
-            } else if (cashBack >= 2.0f) {
-                change.append("TWO");
-                cashBack -= 2.0f;
-            } else if (cashBack >= 1.0f) {
-                change.append("ONE");
-                cashBack -= 1.0f;
-            } else if (cashBack >= 0.5f) {
-                change.append("HALF DOLLAR");
-                cashBack -= 0.5f;
-            } else if (cashBack >= 0.25f) {
-                change.append("QUARTER");
-                cashBack -= 0.25f;
-            } else if (cashBack >= 0.1f) {
-                change.append("DIME");
-                cashBack -= 0.1f;
-            } else if (cashBack >= 0.05f) {
-                change.append("NICKEL");
-                cashBack -= 0.05f;
-            } else {
-                change.append("PENNY");
-                cashBack -= 0.01f;
-            }
-            change.append(",");
-        }
-        change.setLength(change.length() - 1);
+		for (Denomination d : Denomination.values()) {
+   			while (cashBack >= d.getValue()) {
+        		cashBack -= d.getValue();
+        		change.append(d).append(',');
+    		}
+		}
+		change.setLength(change.length() - 1);
 
-        return change.toString();
-    }
+		return change.toString();		
+	}
+
+	private enum Denomination {
+	    ONE_HUNDRED(100.00f),
+	          FIFTY( 50.00f),
+	         TWENTY( 20.00f),
+	            TEN( 10.00f),
+	           FIVE(  5.00f),
+	            TWO(  2.00f),
+	            ONE(  1.00f),
+	    HALF_DOLLAR(  0.50f),
+	        QUARTER(  0.25f),
+	           DIME(  0.10f),
+	         NICKEL(  0.05f),
+	          PENNY(  0.01f);
+
+	    private final float value;
+
+	    Denomination(float value) {
+	        this.value = value;
+	    }
+
+	    public float getValue() {
+	        return this.value;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return this.name().replace("_", " ");
+	    }
+	}
+}
