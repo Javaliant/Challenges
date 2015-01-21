3bce2e8ed516074dbbfe67be6bdc11c6beacbb98
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-21 12:32:05 -0500

    Create SumIntegers.java

---
+++ SumIntegers.java
@@ -0,0 +1,23 @@
+/* Author: Luigi Vincent
+Challenge: Sum integers from a file.
+Specifications: The first argument to the program will be a path to a filename
+containing a positive integer, one per line. Print the sum of all integers.
+
+Found on CodeEval
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SumIntegers {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File(args[0]);
+		Scanner fileInput = new Scanner(file);
+		int sum = 0;
+		while (fileInput.hasNextLine()) {
+			sum += Integer.parseInt(fileInput.nextLine());
+		}
+		System.out.println(sum);
+	}
+}
