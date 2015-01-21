457e16c57d45ba10ba1db526dd784442cf11dd3e
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-21 11:58:30 -0500

    Create SumDigits.java

---
+++ SumDigits.java
@@ -0,0 +1,31 @@
+/* Author: Luigi Vincent
+Challenge: Sum digits.
+
+Specifications: Given a positive integer, find the sum of its constituent digits
+Your program should accept a file as its argument.
+
+Found on CodeEval
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SumDigits {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File(args[0]);
+		Scanner fileInput = new Scanner(file);
+
+		while (fileInput.hasNextLine()) {
+			printSum(fileInput.nextLine());
+		}
+	}
+
+	private static void printSum(String line) {
+		int sum = 0;
+		for (Character c : line.toCharArray()) {
+			sum += Character.getNumericValue(c);
+		}
+		System.out.println(sum);
+	}
+}
