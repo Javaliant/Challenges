6fcf3b7a84bd755a3f6df784aae8ebdb64c6e1d7
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-22 02:05:30 -0500

    Create SortingNumbers.java

---
+++ SortingNumbers.java
@@ -0,0 +1,43 @@
+/* Author: Luigi Vincent
+
+Challenge: Write a program which sorts numbers
+
+Specifications: Accept a path to a file as the first argument of the program.
+Please note, that you need to print the numbers till the 3rd digit after 
+the dot including trailing zeros.
+
+Found on CodeEval
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SortingNumbers {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File(args[0]);
+		Scanner fileInput = new Scanner(file);
+
+		while(fileInput.hasNextLine()) {
+			printSortedNumbers(fileInput.nextLine());
+		}
+	}
+
+	private static void printSortedNumbers(String line) {
+		String[] numbers = line.split(" ");
+		Set<Float> sorted = new TreeSet<>();
+		StringBuilder sortedNumbers = new StringBuilder();
+
+		for (String s : numbers) {
+			sorted.add(Float.parseFloat(s));
+		}
+		for (float f : sorted) {
+			sortedNumbers.append(f).append(' ');
+		}
+		sortedNumbers.setLength(sortedNumbers.length() - 1);
+
+		System.out.println(sortedNumbers.toString());
+	}
+}
