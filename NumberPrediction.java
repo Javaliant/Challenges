5ca77f07538bc3c04f3f5c97ea6542006d076491
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-24 19:09:17 -0500

    Update NumberPrediction.java

--- NumberPrediction.java
+++ NumberPrediction.java
@@ -8,7 +8,6 @@ import java.io.FileNotFoundException;
 
 public class FileSize {
 	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File(args[0]);
-		System.out.println(file.length());
+		System.out.println(new File(args[0]).length());
 	}
 }
