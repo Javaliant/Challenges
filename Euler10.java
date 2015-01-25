c5e1c34b0d0766c5bdec7df5b8da5f4fe31715f1
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-25 13:39:44 -0500

    Update Euler10.java

--- Euler10.java
+++ Euler10.java
@@ -5,21 +5,23 @@ Find the sum of all the primes below two million.
 */
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Euler10 {
 	public static void main(String[] args) {
 		final long START = System.nanoTime();
+		final int limit = 2000000;
 		long result = 0;
+		
+		List<Integer> primes = new ArrayList<>();
 
-		ArrayList<Integer> nums = new ArrayList<>();
-
-		for (int i = 1; i <= 2000000; i++) {
+		for (int i = 1; i < limit; i++) {
 			if (isPrime(i)) {
-				nums.add(i);
+				primes.add(i);
 			}
 		}
 		
-		for (int n : nums) {
+		for (int n : primes) {
 			result += n;
 		}
 		
@@ -41,5 +43,6 @@ public class Euler10 {
       		if (num % i == 0 || num % (i + 2) == 0) { return false; }
     	}
     	return true;
-  }
+  	}
 }
+	
