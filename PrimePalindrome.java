4f142384287e5ec76bbe91770fe5a245bf62710a
Luigi Vincent <luigivincenthr@gmail.com>
2015-02-05 15:12:47 -0500

    Update PrimePalindrome.java

--- PrimePalindrome.java
+++ PrimePalindrome.java
@@ -7,54 +7,59 @@ Found on CodeEval
 */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
 
 public class PrimePalindrome {
 	public static void main(String[] args) {
-		final int limit = 1000;
+		final int LIMIT = 1000;
 
-		System.out.println(
-			calculateLargestPalindrome(getPrimesUnder(limit))
-		);	
+		System.out.println(computeLargestPrimePalindrome(LIMIT));	
 	}
 
 	private static boolean isPalindromic(int s) {
         return s == reverse(s);
 	}
 
-	private static int reverse(int n) {
-	    return Integer.parseInt(
-	        new StringBuilder(Integer.toString(n)).reverse().toString()
-	    );
+	private static int reverse(int p) {
+    	int reverse = 0;
+
+    	while (p != 0) {
+	        reverse *= 10;
+	        reverse += (p % 10);
+	        p /= 10;
+    	}
+
+	    return reverse;
 	}
 
 	/* Finding all prime numbers under an integer,
 	using Sieve of Eratosthenes
  	http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes */
-	private static List<Integer> getPrimesUnder(int limit) {
-	    int prime;
-	    List<Integer> primes = new ArrayList<>();
-	    List<Integer> numbers = new ArrayList<>();
+	private static List<Integer> getPrimesWithin(int limit) {
+		boolean[] sieve = new boolean[limit];
+		Arrays.fill(sieve, true);
+		List<Integer> primes = new ArrayList<>();
 
-	    for (int i = 2; i < limit; i++) {
-	    	numbers.add(i);
-	    }
+		for (int prime = 2; prime < sieve.length; prime++) {
+    		if (sieve[prime]) {
+    			primes.add(prime);
+        		for (int np = prime * 2; np < sieve.length; np += prime) {
+            		sieve[np] = false;
+        		}
+        	}
+    	}
 
-	    while (!numbers.isEmpty()) {
-	    	prime = numbers.get(0);
-	    	primes.add(prime);
-	    	for (int i = prime; i < limit; i += prime) {
-	    		numbers.remove((Integer)(i));
-	    	}
-	    }
 	    return primes;
 	}
 
-	private static int calculateLargestPalindrome(List list) {
-		ListIterator li = list.listIterator(list.size());
+	private static int computeLargestPrimePalindrome(int limit) {
+		List<Integer> primes = getPrimesWithin(limit);
+		ListIterator li = primes.listIterator(primes.size());
+
 		while (li.hasPrevious()) {
-			if (isPalindromic((int)li.previous())) {
+			if (isPalindromic((int) li.previous())) {
 				return (int) li.next();
 			}
 		}
