aa1bfcaaedbdece1d079800bf8c0318ec2f2ad09
Luigi Vincent <luigivincenthr@gmail.com>
2015-02-09 23:44:40 -0500

    Create ArrayAbsurdityJava8.java

---
+++ ArrayAbsurdityJava8.java
@@ -0,0 +1,41 @@
+/* Author: Luigi Vincent
+
+Challenge: Find the duplicated entry.
+
+Specifications:
+Your program should accept as its first argument a path to a filename.
+Each line in this file is one test case.
+Ignore all empty lines.
+
+Each line begins with a positive integer(N), the size of the array,
+then a semicolon followed by a comma separated list of positive numbers
+which range from 0 to N-2, inclusive.
+
+The array contains exactly one duplicated entry.
+That duplicate appears exactly twice.
+Print out the duplicated entry, each one on a new line.
+
+Found on CodeEval, java 8 implementation suggested by rolfl (CodeReview)
+*/
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+public class ArrayAbsurdityJava8 {
+    public static void main(String[] args) throws IOException {
+        Files.lines(new File(args[0]).toPath())
+            .filter(s -> !s.isEmpty())
+            .forEach(ArrayAbsurdityJava8::printDuplicate);
+    }
+
+    private static void printDuplicate(String line) {
+      int length = Integer.parseInt(line.split(";")[0]);
+      int sum = Arrays.stream(line.split(";")[1].split(","))
+                      .mapToInt(Integer::parseInt)
+                      .sum();
+
+      System.out.println(sum - (length - 1) * (length - 2) / 2);
+    }
+}
